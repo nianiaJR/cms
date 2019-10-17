b8d9f7db326207e3ecbe746a4baa05fb94e0cdac
nianiaJR <ljracm@gmail.com>
2019-10-17 23:41:31 +0800

    feat: add router && login init

--- app/assets/App.tsx
+++ app/assets/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { Route, withRouter, Switch, Redirect } from 'react-router-dom'
 import { hot } from 'react-hot-loader/root'
+import { Layout } from 'antd'
+
+import Login from './modules/Login'
+
+const { Header, Content } = Layout
 
 const App = () => (
-  <div>
-    <h3>Hello world</h3>
-    <p>这是一个 egg + react 的全栈应用，前端基于webpack构建</p>
-  </div>
+  <Layout>
+    <Header>
+    </Header>
+    <Content>
+      {/* <div className="beauty-cms">
+        <h3>Hello world</h3>
+        <p>这是一个 egg + react 的全栈应用，前端基于webpack构建</p>
+      </div> */}
+      <Switch>
+        <Route path="/" component={Login}></Route>
+        <Redirect to="/" />
+      </Switch>
+    </Content>
+  </Layout>
 )
 
-export default hot(App)
+export default withRouter(hot(App))
---
+++ app/assets/modules/Login/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default class Login extends React.Component<{}, {}> {
+  constructor(props:any) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className="login">
+        登录
+      </div>
+    )
+  }
+}
